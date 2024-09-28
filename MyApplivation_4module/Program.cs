4aa905d14c7fdd31881a9f2a3e567a2e650d03e6
Ведерников Сергей <swnike@mail.ru>
2024-09-28 22:00:39 +0500

    Добавьте файлы проекта.

---
+++ MyApplivation_4module/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MyApplivation_4module
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String A;
+            String B;
+            bool C;
+
+            C = A != B;
+
+        }
+    }
+}
