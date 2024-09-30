5884e3e34459be7b6c797f32420a7e4bb58f37da
Ведерников Сергей <swnike@mail.ru>
2024-09-30 22:34:12 +0500

    Задание 4.3.7

--- MyApplivation_4module/Program.cs
+++ MyApplivation_4module/Program.cs
@@ -17,6 +17,11 @@
 
             Console.WriteLine("Последняя буква вашего имени: {0}", name[name.Length - 1]);
 
+            for (int i = name.Length-1; i >= 0; i--)
+            {
+                Console.Write(name[i] + "");
+            }
+
             Console.ReadKey();
         }
     }
